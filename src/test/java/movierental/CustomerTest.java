24ee517c8700d1dbac1fc0f49b3899f5a3126cb7
Ivan Darmawan <ivandarmawan@users.noreply.github.com>
2018-11-27 12:21:28 +0700

    Add test() - I know it was a bad method name :)

--- src/test/java/movierental/CustomerTest.java
+++ src/test/java/movierental/CustomerTest.java
@@ -7,6 +7,28 @@ import org.junit.Test;
 public class CustomerTest {
 
     @Test
+    public void test() {
+        Customer customer = new Customer("Bob");
+        
+        customer.addRental(new Rental(new Movie("Jaws", Movie.REGULAR), 2));
+        customer.addRental(new Rental(new Movie("GoldenEye", Movie.REGULAR), 3));
+        customer.addRental(new Rental(new Movie("ShortNew", Movie.NEW_RELEASE), 1));
+        customer.addRental(new Rental(new Movie("LongNew", Movie.NEW_RELEASE), 2));
+        customer.addRental(new Rental(new Movie("Bambi", Movie.CHILDRENS), 3));
+        customer.addRental(new Rental(new Movie("Toy Story", Movie.CHILDRENS), 4));
+        
+        assertEquals("Rental Record for Bob\n" +
+                "\tJaws\t2.0\n" +
+                "\tGoldenEye\t3.5\n" +
+                "\tShortNew\t3.0\n" +
+                "\tLongNew\t6.0\n" +
+                "\tBambi\t1.5\n" + 
+                "\tToy Story\t3.0\n" + 
+                "Amount owed is 19.0\n" +
+                "You earned 7 frequent renter points", customer.statement());
+    }    
+    
+    @Test
     public void testCustomer() {
         Customer c = new CustomerBuilder().build();
         assertNotNull(c);
